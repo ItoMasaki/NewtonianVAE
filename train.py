a839e7b5bb35ff64521f342505690024b6bcf034
Masaki Ito <ito.masaki@em.ci.ritsumei.ac.jp>
2022-12-10 16:40:04 +0900

    [Update] check test data

--- train.py
+++ train.py
@@ -57,8 +57,6 @@ model = NewtonianVAE(**cfg["model"])
 if cfg["load_model"]:
     model.load(cfg["load_model_path"], cfg["load_model_file"])
 
-
-test_loss: float = 0.
 best_loss: float = 1e32
 beta: float = 0.001
 
@@ -67,51 +65,68 @@ with tqdm(range(1, cfg["epoch_size"]+1)) as pbar:
     for epoch in pbar:
         pbar.set_description(f"[Epoch {epoch}]")
 
+        train_loss: float = 0.
+        validation_loss: float = 0.
+        test_loss: float = 0.
+
         # for idx, (I, u, _) in enumerate(train_loader):
+
         for idx, (I, u) in enumerate(train_loader):
-            train_loss = model.train(
+            train_loss += model.train(
                 {"I": I.permute(1, 0, 2, 3, 4), "u": u.permute(1, 0, 2), "beta": beta})
-            writer.add_scalar('train_loss', train_loss, epoch - 1)
-            pbar.set_postfix({"test": test_loss, "train": train_loss})
+
+        writer.add_scalar('train_loss', train_loss /
+                          cfg["dataset"]["train"]["episode_size"], epoch - 1)
 
         # for I, u, _ in test_loader:
-        for I, u in validation_loader:
-            test_loss = model.test(
+        for idx, (I, u) in enumerate(validation_loader):
+            validation_loss += model.test(
                 {"I": I.permute(1, 0, 2, 3, 4), "u": u.permute(1, 0, 2), "beta": beta})
-            writer.add_scalar('test_loss', test_loss, epoch - 1)
-            pbar.set_postfix({"test": test_loss, "train": train_loss})
+
+        writer.add_scalar('validation_loss', validation_loss /
+                          cfg["dataset"]["validation"]["episode_size"], epoch - 1)
+
+        for idx, (I, u) in enumerate(test_loader):
+            test_loss += model.test(
+                {"I": I.permute(1, 0, 2, 3, 4), "u": u.permute(1, 0, 2), "beta": beta})
+
+        writer.add_scalar('test_loss', test_loss /
+                          cfg["dataset"]["test"]["episode_size"], epoch - 1)
+
+        pbar.set_postfix({"validation": validation_loss/cfg["dataset"]["validation"]["episode_size"],
+                          "train": train_loss/cfg["dataset"]["train"]["episode_size"],
+                          "test": test_loss/cfg["dataset"]["test"]["episode_size"]})
 
         model.save(f"{save_weight_path}", f"{epoch}.weight")
-        model.save_ckpt(f"{save_weight_path}", f"train.ckpt", epoch, test_loss)
+        model.save_ckpt(f"{save_weight_path}",
+                        f"train.ckpt", epoch, validation_loss)
 
-        if test_loss < best_loss:
+        if validation_loss < best_loss:
             model.save(f"{save_weight_path}", f"best.weight")
-            best_loss = test_loss
+            best_loss = validation_loss
 
         if 30 <= epoch and epoch < 60:
             beta += 0.0333
 
         if epoch % cfg["check_epoch"] == 0:
 
-            for I, u in test_loader:
+            all_positions: list = []
 
-                all_positions: list = []
+            for step in range(0, cfg["dataset"]["train"]["sequence_size"]-1):
 
-                for step in range(0, cfg["dataset"]["train"]["sequence_size"]-1):
+                I_t, I_tp1, x_q_t, x_p_tp1 = model.estimate(I.permute(1, 0, 2, 3, 4)[
+                                                            step+1], I.permute(1, 0, 2, 3, 4)[step], u.permute(1, 0, 2)[step+1])
 
-                    I_t, I_tp1, x_q_t, x_p_tp1 = model.estimate(I.permute(1, 0, 2, 3, 4)[
-                                                                step+1], I.permute(1, 0, 2, 3, 4)[step], u.permute(1, 0, 2)[step+1])
+                all_positions.append(
+                    x_q_t.to("cpu").detach().numpy()[0].tolist())
 
-                    all_positions.append(
-                        x_q_t.to("cpu").detach().numpy()[0].tolist())
+                visualizer.append(
+                    I.permute(1, 0, 2, 3, 4)[step].to(
+                        "cpu").detach().numpy()[0].transpose(1, 2, 0),
+                    I_t.to("cpu").detach().to(torch.float32).numpy()[
+                        0].transpose(1, 2, 0),
+                    np.array(all_positions)
+                )
 
-                    visualizer.append(
-                        I.permute(1, 0, 2, 3, 4)[step].to(
-                            "cpu").detach().numpy()[0].transpose(1, 2, 0),
-                        I_t.to("cpu").detach().to(torch.float32).numpy()[
-                            0].transpose(1, 2, 0),
-                        np.array(all_positions)
-                    )
-
-                visualizer.encode(save_video_path, f"{epoch}.mp4")
-                print()
+            visualizer.encode(save_video_path, f"{epoch}.{idx}.mp4")
+            print()
